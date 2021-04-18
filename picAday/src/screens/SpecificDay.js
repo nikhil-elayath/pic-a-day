da3c2c8cfe1e6b3e912a5a19472e6ba258e06a9d
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-18 22:35:26 +0530

    created layout for specific day screen

--- picAday/src/screens/SpecificDay.js
+++ picAday/src/screens/SpecificDay.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import ImageCard from "../components/ImageCard"
+import {homepage} from '../assests/Icons';
+import HomeStyles from "../assests/styles/components/Home"
+import Header from "../reuseableComponents/Header"
+
+
 
 export default function SpecificDay() {
     return (
         <View>
-            <Text>SpecificDay</Text>
+            <Header showBackButton={true}/>
+            <Text> specific</Text>
+            <View style={HomeStyles.imageContainer}>
+            <ImageCard 
+            imageSource={homepage.dummyImage}
+            location={"Kochi, India"}
+            date={"Jan 18"}
+            temperature={"24"}
+            />
+           
+            </View>
+            <View>
+                <Text> Description for the iamge above</Text>
+                </View>
+            
         </View>
     )
 }
