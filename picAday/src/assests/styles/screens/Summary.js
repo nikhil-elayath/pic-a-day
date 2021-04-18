859ecc30121917c27ba4a4a1ab2c6929afeff937
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-18 22:01:33 +0530

    added sty;esheet for summary screen

---
+++ picAday/src/assests/styles/screens/Summary.js
@@ -0,0 +1,44 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+
+const SummaryStyles = StyleSheet.create({
+    mainContainer:{
+
+alignItems:"center",
+// backgroundColor:"red",
+
+    },
+    streakContainer:{
+        width:"100%",
+
+    alignItems:"center",
+    backgroundColor:"#F8F9F9",
+    borderBottomWidth :1,
+    borderBottomColor: '#E4E4E5',
+
+
+    },
+    hottestDayContainer:{
+        width:"100%",
+
+    alignItems:"center",
+    backgroundColor:"#F8F9F9",
+    borderBottomWidth :1,
+    borderBottomColor: '#E4E4E5',
+    
+
+
+    },
+    coldestDayContainer:{
+        width:"100%",
+    alignItems:"center",
+    backgroundColor:"#F8F9F9",
+    borderBottomWidth :1,
+    borderBottomColor: '#E4E4E5',
+
+    },
+  
+
+
+})
+export default SummaryStyles;
--- picAday/src/screens/Summary.js
+++ picAday/src/screens/Summary.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import Header from "../reuseableComponents/Header"
+import SummaryStyles from "../assests/styles/screens/Summary"
 
 
 export default function Summary() {
     return (
-        <View>
+        <>
             <Header showBackButton={true}/>
-            <Text>Summary</Text>
-        </View>
+        <View style={SummaryStyles.mainContainer}>
+           
+            <View style={SummaryStyles.streakContainer}>
+            <View>
+
+                <Text>Days </Text>
+                </View>
+                <View>
+                <Text>17/19 </Text>
+                </View>
+                <View>
+                <Text>You have recorded since 17 days </Text>
+                </View>
+            </View>
+            
+            <View style={SummaryStyles.hottestDayContainer}>
+            <View>
+                <Text>Hottest Day </Text>
+                <Text>30C </Text>
+                <Text>Sunday  </Text>
+                </View>
+            </View>
+            <View style={SummaryStyles.coldestDayContainer}>
+            <View>
+                <Text>Codlewst day Day </Text>
+                <Text>30C </Text>
+                <Text>Sunday  </Text>
+                </View>
+            </View>
+            </View>
+            
+            </>
     )
 }
