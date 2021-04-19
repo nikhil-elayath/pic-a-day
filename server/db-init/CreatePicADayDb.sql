468dc51ef013d7308a1f7fd9d5da2959563dd6f2
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-19 21:00:15 +0530

    get api to retrieve all the data from db

--- server/db-init/CreatePicADayDb.sql
+++ server/db-init/CreatePicADayDb.sql
@@ -9,7 +9,7 @@ CREATE TABLE user_entry
     image_uri VARCHAR(255),
     image_description VARCHAR(255),
     entry_date DATE,
-    locaction VARCHAR(255),
+    image_location VARCHAR(255),
     temperature VARCHAR(255),
     PRIMARY KEY(id)
 
--- server/routes/Entry.js
+++ server/routes/Entry.js
@@ -7,21 +7,22 @@ const db = require("../db-init/dbConn");
 // api to get all the user entry 
 router.get("/all-entry", async (req, res, next) => {
     console.log("all entry api")
-    // const userId = req.params.id;
+   
     try {
       const result = await db.any(
         `select * from user_entry`
       );
+      console.log("result", result)
       if (result.length == 0) {
         res.status(400).json({
           status: 400,
-          message: "No MarinaX Card found",
+          message: "No Entry found",
         });
       } else {
         res.status(200).json({
           status: 200,
-          message: "MarinaX card details fetched successfully",
-          data: { result, user },
+          message: "Fecthed all entry successfully",
+          data: { result },
         });
       }
     } catch (error) {
