dfc0a68d04220afd1f66a4d08d7060974fe38fda
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-24 15:02:43 +0530

    getting the location and temperature as soon as image is taken

--- picAday/src/screens/CaptureImage.js
+++ picAday/src/screens/CaptureImage.js
@@ -1,13 +1,67 @@
 import React, {useEffect, useRef} from 'react';
 import {View, Text, Platform, TouchableOpacity, Alert} from 'react-native';
 import {RNCamera as Camera} from 'react-native-camera';
+import {createUserEntry, updateUserEntryById} from '../actions/UserEntry';
+import Geolocation from 'react-native-geolocation-service';
+
+
 import ImageCard from '../components/ImageCard';
 import BottomTabBar from '../reuseableComponents/BottomTabBar';
+import Geolocation from 'react-native-geolocation-service';
+import {useDispatch, useSelector} from 'react-redux';
 
 export default function CaptureImage(props) {
-  // const [imgUrl, setImgUrl] = useState(null);
-
   const cameraRef = useRef();
+  const dispatch = useDispatch();
+
+  const getImageLocationAndTemperature = async imageUri => {
+    console.log('start', imageUri);
+    try {
+      var imageLocation;
+      console.log("within try")
+
+      // getting the location
+     await Geolocation.getCurrentPosition(async position => {
+       console.log("within geolocation")
+        const url = `https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=VQCnCJVY_c9-7ezRY5xlhmh_QtFkt8hC-dlYMyc-4Bw&mode=retrieveAddresses&prox=${position.coords.latitude},${position.coords.longitude}`;
+        await fetch(url)
+          .then(res => console.log(res))
+          // .then(async resJson => {
+          //   imageLocation =
+          //     resJson.Response.View[0].Result[0].Location.Address.County +
+          //     ',' +
+          //     resJson.Response.View[0].Result[0].Location.Address
+          //       .AdditionalData[0].value;
+          //   console.log('image above', imageLocation);
+          // })
+          .catch(e => {
+            console.log('Error in getAddressFromCoordinates', e);
+          });
+      });
+
+      // getting temperature
+      const openweather_api =
+        'https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=cad6b217b338049c1700977e5c93f721&units=metric';
+      fetch(openweather_api)
+        .then(res => res.json())
+        .then(async resJson => {
+          let data = {
+            imageUri: imageUri,
+            imageLocation: imageLocation,
+            imageTemperature: resJson.main.temp,
+          };
+          console.log('printing data', data);
+          await dispatch(createUserEntry(data));
+        })
+        .catch(e => {
+          console.log('Error in ', e);
+        });
+
+      // navigating to day edit view
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const takePicture = async () => {
     console.log('from take picture');
 
@@ -19,16 +73,15 @@ export default function CaptureImage(props) {
 
     try {
       const data = await cameraRef.current.takePictureAsync();
+      await getImageLocationAndTemperature(data.uri);
       //  call to database
-      props.navigation.navigate('DayEditView', {imageUri: data.uri});
+      // props.navigation.navigate('DayEditView', {imageUri: data.uri});
     } catch (err) {
       Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
       return;
     } finally {
     }
   };
-
- 
 
   return (
     <View style={{flex: 1}}>
@@ -54,15 +107,13 @@ export default function CaptureImage(props) {
           onGoogleVisionBarcodesDetected={({barcodes}) => {
             console.log(barcodes);
           }}
-          onPress={takePicture}>
-        </Camera>
+          onPress={takePicture}></Camera>
       </View>
-      <BottomTabBar navigation={props.navigation}
-      cameraOn={true}
-      captureImage={takePicture}
-    
-
-       />
+      <BottomTabBar
+        navigation={props.navigation}
+        cameraOn={true}
+        captureImage={takePicture}
+      />
     </View>
   );
 }
