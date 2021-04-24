545324ed9157716ea9df857c9a8012fbf1367135
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-24 20:22:56 +0530

    formatted code

--- server/routes/Entry.js
+++ server/routes/Entry.js
@@ -69,8 +69,6 @@ router.post("/create-entry", async (req, res, next) => {
     var imageDescription = req.body.imageDescription;
     var imageLocation = req.body.imageLocation;
     var imageTemperature = req.body.imageTemperature;
-    var entryCreationDate= new Date()
-
   } catch (error) {
     res.status(400).json({
       status: 400,
@@ -89,7 +87,6 @@ router.post("/create-entry", async (req, res, next) => {
     if (dy.length < 2) dy = "0" + dy;
 
     let userEntryDate = [yr, mon, dy].join("-");
-    console.log("fimal", final_date)
     const result = await db.any(
       `INSERT INTO user_entry(image_uri, image_description, entry_date, image_location, temperature) VALUES('${imageUri}', '${imageDescription}','${userEntryDate}','${imageLocation}','${imageTemperature}')  RETURNING id`
     );
@@ -123,8 +120,18 @@ router.put("/update-entry", async (req, res, next) => {
   }
 
   try {
+    var date = new Date();
+
+    var mon = "" + (date.getMonth() + 1);
+    var dy = "" + date.getDate();
+    var yr = date.getFullYear();
+
+    if (mon.length < 2) mon = "0" + mon;
+    if (dy.length < 2) dy = "0" + dy;
+
+    let userEntryDate = [yr, mon, dy].join("-");
     const result = await db.any(
-      `update user_entry set image_uri='${imageUri}', image_description='${imageDescription}', entry_date='${imageDescription}' where id = '${userEntryId}';`
+      `update user_entry set image_uri='${imageUri}', image_description='${imageDescription}', entry_date='${userEntryDate}' where id = '${userEntryId}';`
     );
 
     res.status(200).json({
