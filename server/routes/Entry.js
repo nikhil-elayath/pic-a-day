9af437352b8b92fdeee7d91802165e2fb5d98563
Nikhil Elayath <nikhil.elayath13@gmail.com>
2021-04-26 20:54:30 +0530

    remoed console logs fro api

--- server/routes/Entry.js
+++ server/routes/Entry.js
@@ -9,19 +9,12 @@ router.get("/all-entry", async (req, res, next) => {
 
   try {
     const result = await db.any(`select * from user_entry`);
-    console.log("result", result)
-    if (result.length == 0) {
-      res.status(400).json({
-        status: 400,
-        message: "No Entry found",
-      });
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Fecthed all entry successfully",
-        data: { result },
-      });
-    }
+
+    res.status(200).json({
+      status: 200,
+      message: "Fecthed all entry successfully",
+      data: { result },
+    });
   } catch (error) {
     res.status(400).json({
       status: 400,
@@ -39,19 +32,12 @@ router.get("/get-entry/:id", async (req, res, next) => {
     const result = await db.any(
       `select * from user_entry where id = '${userEntryId}';`
     );
-    console.log("result", result);
-    if (result.length == 0) {
-      res.status(400).json({
-        status: 400,
-        message: "No Entry found",
-      });
-    } else {
-      res.status(200).json({
-        status: 200,
-        message: "Fecthed all entry successfully",
-        data: { result },
-      });
-    }
+
+    res.status(200).json({
+      status: 200,
+      message: "Fecthed all entry successfully",
+      data: { result },
+    });
   } catch (error) {
     res.status(400).json({
       status: 400,
@@ -78,16 +64,7 @@ router.post("/create-entry", async (req, res, next) => {
 
   try {
     var dateValue = new Date().toISOString();
-    console.log("date", dateValue)
 
-    // var mon = "" + (date.getMonth() );
-    // var dy = "" + (date.getDate() );
-    // var yr = date.getFullYear();
-
-    // if (mon.length < 2) mon = "0" + mon;
-    // if (dy.length < 2) dy = "0" + dy;
-
-    // let userEntryDate = [yr, mon, dy].join("-");
     const result = await db.any(
       `INSERT INTO user_entry(image_uri, image_description, entry_date, image_location, temperature) VALUES('${imageUri}', '${imageDescription}','${dateValue}','${imageLocation}','${imageTemperature}')  RETURNING id`
     );
@@ -121,24 +98,15 @@ router.put("/update-entry", async (req, res, next) => {
   }
 
   try {
-    var date = new Date();
+    var dateValue = new Date().toISOString();
 
-    var mon = "" + (date.getMonth() + 1);
-    var dy = "" + date.getDate();
-    var yr = date.getFullYear();
-
-    if (mon.length < 2) mon = "0" + mon;
-    if (dy.length < 2) dy = "0" + dy;
-
-    let userEntryDate = [yr, mon, dy].join("-");
     const result = await db.any(
-      `update user_entry set image_uri='${imageUri}', image_description='${imageDescription}', entry_date='${userEntryDate}' where id = '${userEntryId}';`
+      `update user_entry set image_uri='${imageUri}', image_description='${imageDescription}', entry_date='${dateValue}' where id = '${userEntryId}';`
     );
 
     res.status(200).json({
       status: 200,
       message: "Created entry successfully",
-      // data:result[0]
     });
   } catch (error) {
     console.log(error);
@@ -239,7 +207,6 @@ router.get("/get-summary", async (req, res, next) => {
           yearOfTheEntry,
       };
     }
-    console.log("lowestTemperatureData", lowestTemperatureData);
 
     res.status(200).json({
       status: 200,
